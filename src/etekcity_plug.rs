612fe2d26e4c7b3000a517f218b50f8d748bf2a9
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-11-09 09:23:49 -0500

    Return enum for on/off fns

--- src/etekcity_plug.rs
+++ src/etekcity_plug.rs
@@ -1,40 +1,81 @@
-use vesync::{Status, VeSyncAccount, VeSyncDevice};
-use std::env;
 use anyhow;
+use std::env;
+use vesync::{Status, VeSyncAccount, VeSyncDevice};
 
 const VESYNC_ACCOUNT: &str = env!("VESYNC_ACCOUNT");
 const VESYNC_KEY: &str = env!("VESYNC_KEY");
 
-pub struct EtekcityPlug<'a> {
-    plug: VeSyncDevice<'a>,
+pub struct EtekcityPlug {
+    account: VeSyncAccount,
 }
 
-impl EtekcityPlug<'_> {
-    fn new() -> Result<EtekcityPlug<'static>, anyhow::Error> {
-        let account = VeSyncAccount::login(VESYNC_ACCOUNT, VESYNC_KEY);
-        let devices = account.unwrap().get_devices().unwrap();
-        
-        let plug = devices
-        .iter()
-        .find(|device| device.cid == "Black floor lamp").unwrap();
-        let plug = *plug;
-        Ok(EtekcityPlug { plug })
-    }
-
-    fn on(&self) -> Option<()> {
-       match self.plug.deviceStatus {
-          Status::Off => self.plug.device_toggle(),
-          _ => Ok(()),
-       };
-       Some(())
-    }
-    
-    fn off(&self) -> Option<()> {
-        match self.plug.deviceStatus {
-            Status::On => self.plug.device_toggle(),
-            _ => Ok(()),
-        };
-        Some(())
-    }
+enum ToggleResult {
+    TurnedOn,
+    LeftOn,
+    TurnedOff,
+    LeftOff,
+    ToggledBlindly,
+    Error,
+    PlugNotFound,
 }
 
+impl EtekcityPlug {
+    fn new() -> Result<EtekcityPlug, anyhow::Error> {
+        let account = VeSyncAccount::login(VESYNC_ACCOUNT, VESYNC_KEY).unwrap();
+        Ok(EtekcityPlug { account })
+    }
+
+    fn on(&self) -> ToggleResult {
+        let devices = self.account.get_devices().unwrap();
+
+        let search_result = devices
+            .into_iter()
+            .find(|device| device.cid == "Black floor lamp");
+
+        if let Some(mut plug) = search_result {
+            match plug.deviceStatus {
+                Status::Off => match plug.device_toggle() {
+                    Ok(_) => ToggleResult::TurnedOn,
+                    Err(_) => ToggleResult::Error,
+                },
+                Status::On => ToggleResult::LeftOn,
+                Status::Unknown => {
+                    if let Err(_) = plug.device_toggle() {
+                        ToggleResult::Error
+                    } else {
+                        ToggleResult::ToggledBlindly
+                    }
+                }
+            }
+        } else {
+            ToggleResult::PlugNotFound
+        }
+    }
+
+    fn off(&self) -> ToggleResult {
+        let devices = self.account.get_devices().unwrap();
+
+        let search_result = devices
+            .into_iter()
+            .find(|device| device.cid == "Black floor lamp");
+
+        if let Some(mut plug) = search_result {
+            match plug.deviceStatus {
+                Status::On => match plug.device_toggle() {
+                    Ok(_) => ToggleResult::TurnedOff,
+                    Err(_) => ToggleResult::Error,
+                },
+                Status::Off => ToggleResult::LeftOff,
+                Status::Unknown => {
+                    if let Err(_) = plug.device_toggle() {
+                        ToggleResult::Error
+                    } else {
+                        ToggleResult::ToggledBlindly
+                    }
+                }
+            }
+        } else {
+            ToggleResult::PlugNotFound
+        }
+    }
+}
